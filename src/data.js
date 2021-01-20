4266dd65612d737e4bb80b0d12a54d1cc4a6fc0e
rajkamal3 <rkchenumalla@gmail.com>
2021-01-21 01:01:18 +0530

    experience added

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const experience = [
+    {
+        'id': 'recAGJfiU4CeaV0HL',
+        'order': 3,
+        'title': 'Full Stack Web Developer',
+        'dates': 'September 2018 - Present',
+        'duties': [
+            'Developed web applications that processed, analyzed and rendered data visually.',
+            'Increased the speed of fetch transactions page by 150x by rewriting the entire functionality in JavaScript.',
+            'Optimized service calls from the client-side by implementing caching and localStorage.',
+            'Designed and developed loading animations with CSS which represented the Synchrony brand.',
+            'Partnered with the React team in developing the ground-up redesign of our app.',
+            'Collaborated with the design team for pixel-perfect implementation of our approved user interface and made sure that design decisions always resulted in high performant functional outcomes.',
+            'Collaborated with product owners and on-site coordinator and made sure that new changes and additions always acted in the betterment of the application.',
+            'Served as the face of the frontend team by following high standards in application development.',
+            'Lead the two teams I worked on in developing the UI functionalities.',
+            'Modernized and fixed many long-time drawbacks and defects of our applications.',
+            'Mentored junior and mid-level teammates on front-end programming.',
+            'Managed time-sensitive updates, including content changes and performance upgrades.'
+        ],
+        'company': 'Capgemini'
+    },
+    {
+        'id': 'recIL6mJNfWObonls',
+        'order': 2,
+        'title': 'Frontend Developer & Designer Intern',
+        'dates': 'May 2018 - August 2018',
+        'duties': [
+            'Designed the entire user interface of our mobile app and website from scratch.',
+            'Designed several user interfaces with great attention to user experience and illustrated graphic designs, icons for our app and social media marketing.',
+            'Played a major role in product development, frontend web development and brainstorming sessions.',
+            'Created new marketing templates and made s ure that brand recognition is effective.'
+        ],
+        'company': 'Starein'
+    },
+    {
+        'id': 'rec61x18GVY99hQq5',
+        'order': 1,
+        'title': 'Graphic Designer Marketing Intern',
+        'dates': 'August 2018 - September 2018',
+        'duties': [
+            'Designed social media marketing templates for improving the reach of our product.',
+            'Partnered with other interns in brain-storming sessions in improving the brand image of the company.',
+            'Designed and improved the logos and brand related stuff of the company.'
+        ],
+        'company': 'Butterfly Edu.'
+    }
+];
